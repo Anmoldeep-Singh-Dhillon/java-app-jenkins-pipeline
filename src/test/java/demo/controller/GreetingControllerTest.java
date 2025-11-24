f92bbde9f9c15b7cbd89b3252aac38368f7fe088
apple <apple@Anmols-MacBook-Pro.local>
2025-11-24 17:24:00 +0530

    first commit

--- src/test/java/demo/controller/GreetingControllerTest.java
+++ src/test/java/demo/controller/GreetingControllerTest.java
@@ -1,6 +1,6 @@
 package demo.controller;
 
-import com.example.demo.service.GreetingService;
+import demo.service.GreetingService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
